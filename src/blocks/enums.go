0812497d6d9eaaccee9549056222fcfdac3338fe
Israel van der Roest <119486165+Israelvdr@users.noreply.github.com>
2026-08-03 01:21:05 +0800

    feat(blocks): add block data structures and initial constructors (#2)
    Add src directory with blocks package.
    Add blocks.go with data structures for elements, blocks, rounds, block designs, etc.
    Add basic constructors for types.
    Add some basic functions.
    Add supporting types and functions such as errors and enums.
    
    
    Issue: 1
    
    Signed-off-by: Israel van der Roest <israelvanderroest@outlook.com>

---
+++ src/blocks/enums.go
@@ -0,0 +1,45 @@
+package blocks
+
+const (
+	ELEMENT_ALWAYS_FIRST string = "element is always first"
+	ELEMENT_NULL         string = "null element"
+	ELEMENT_STANDARD     string = "standard element"
+)
+
+type elementCharacteristic interface {
+	Characteristic() string
+	Equals(elementCharacteristic) bool
+}
+
+type characteristicAlwaysFirst struct {
+}
+
+func (b characteristicAlwaysFirst) Characteristic() string {
+	return ELEMENT_ALWAYS_FIRST
+}
+func (b characteristicAlwaysFirst) Equals(comp elementCharacteristic) bool {
+	_, ok := comp.(characteristicAlwaysFirst)
+	return ok
+}
+
+type characteristicNull struct {
+}
+
+func (b characteristicNull) Characteristic() string {
+	return ELEMENT_NULL
+}
+func (b characteristicNull) Equals(comp elementCharacteristic) bool {
+	_, ok := comp.(characteristicNull)
+	return ok
+}
+
+type characteristicStandard struct {
+}
+
+func (b characteristicStandard) Characteristic() string {
+	return ELEMENT_STANDARD
+}
+func (b characteristicStandard) Equals(comp elementCharacteristic) bool {
+	_, ok := comp.(characteristicStandard)
+	return ok
+}
